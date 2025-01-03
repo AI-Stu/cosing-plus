6c9394eeeee163c843e562e2744f8eb4e6c04644
huguanghao <1599264046@qq.com>
2025-01-03 09:43:04 +0800

    fix: 子页面样式调整

--- src/pages/exception/component-error.vue
+++ src/pages/exception/component-error.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-result status="404" title="页面配置错误" sub-title="动态配置页面不存在，请检查配置项" />
+  <a-result class="h-full bg-[#fff]" status="404" title="页面配置错误" sub-title="动态配置页面不存在，请检查配置项" />
 </template>
 
 <script setup lang="ts">
